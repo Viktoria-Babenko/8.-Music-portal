02cba8ba4fc387b81a8c3c9e53c9953eadd8aa79
Виктория <babenko.viktoria.v@gmail.com>
2024-03-10 16:41:37 +0200

    #Task 7 Создание класса контекста MusicPortalContext для задание базы SQL с пакетом Microsoft.EntityFrameworkCore.SqlServer

---
+++ 8. Music portal/Models/MusicPortalContext.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+
+namespace _8._Music_portal.Models
+{
+    public class MusicPortalContext : DbContext
+    {
+        public DbSet<UserModel> Users { get; set; }
+        public DbSet<GenreModel> Genres { get; set; }
+        public DbSet<PerformerModel> Performers { get; set; }
+        public DbSet<SongsModel> Songs { get; set; }
+        public MusicPortalContext(DbContextOptions<MusicPortalContext> options)
+            : base(options)
+        {
+            Database.EnsureCreated();
+        }
+    }
+}
