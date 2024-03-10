5a2cb70e2c932577d8a2d261360f5a4bc740ab29
Виктория <babenko.viktoria.v@gmail.com>
2024-03-10 15:48:30 +0200

    #Task 1 Создание модели LoginModel

---
+++ 8. Music portal/Models/LoginModel.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace _8._Music_portal.Models
+{
+    public class LoginModel
+    {
+        [Required]
+        [Display(Name = "Логин")]
+        public string? Login { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        [Display(Name = "Пароль")]
+        public string? Password { get; set; }
+    }
+}
