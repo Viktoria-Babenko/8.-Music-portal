29f7205cd4fe7ccf6d4ca2673382320c8a06a3a2
Виктория <babenko.viktoria.v@gmail.com>
2024-03-10 15:52:05 +0200

    #Task 2 Создание модели RegisterModel

---
+++ 8. Music portal/Models/RegisterModel.cs
@@ -0,0 +1,35 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace _8._Music_portal.Models
+{
+    public class RegisterModel
+    {
+        [Required]
+        [Display(Name = "Имя")]
+        public string? FirstName { get; set; }
+
+        [Required]
+        [Display(Name = "Фамилия")]
+        public string? LastName { get; set; }
+
+        [Required]
+        [Display(Name = "Логин")]
+        public string? Login { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        [Display(Name = "Пороль")]
+        public string? Password { get; set; }
+
+        [Required]
+        [Compare("Password", ErrorMessage = "Passwords do not match")]
+        [DataType(DataType.Password)]
+        [Display(Name = "Пороль потверждение")]
+        public string? PasswordConfirm { get; set; }
+
+        [Required]
+        [DataType(DataType.EmailAddress)]
+        [Display(Name = "Електронная почта ")]
+        public string? email { get; set; }
+    }
+}
