2caebe6d2dd1f70125f3fc3923e309c969615b59
Виктория <babenko.viktoria.v@gmail.com>
2024-03-10 15:54:52 +0200

    #Task 3 Создание модели UserModel

---
+++ 8. Music portal/Models/UserModel.cs
@@ -0,0 +1,32 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace _8._Music_portal.Models
+{
+    public class UserModel
+    {
+        public int Id { get; set; }
+        [Required(ErrorMessage = "Поле должно быть установлено")]
+        [Display(Name = "Имя")]
+        public string? FirstName { get; set; }
+
+        [Required(ErrorMessage = "Поле должно быть установлено")]
+        [Display(Name = "Фамилия")]
+        public string? LastName { get; set; }
+        [Required(ErrorMessage = "Поле должно быть установлено")]
+        [Display(Name = "Логин")]
+        public string? Login { get; set; }
+
+        [Required(ErrorMessage = "Поле должно быть установлено.")]
+        [DataType(DataType.Password)]
+        [Display(Name = "Пароль")]
+        public string? Password { get; set; }
+        public string? Salt { get; set; }
+
+        [Required]
+        [DataType(DataType.EmailAddress)]
+        [Display(Name = "Електронная почта ")]
+        public string? email { get; set; }
+
+        public int? Status { get; set; }
+    }
+}
