9b90db7163a653957e03f448fcbeaee5b7ea921c
Виктория <babenko.viktoria.v@gmail.com>
2024-03-10 16:30:43 +0200

    #Task 6 Изминение полей Name в классах GenreModel и PerformerModel

--- 8. Music portal/Models/PerformerModel.cs
+++ 8. Music portal/Models/PerformerModel.cs
@@ -3,7 +3,7 @@
     public class PerformerModel
     {
         public int Id { get; set; }
-        public string Name { get; set; }
+        public string? Name { get; set; }
         public ICollection<SongsModel>? Songs { get; set; }
         public PerformerModel()
         {
