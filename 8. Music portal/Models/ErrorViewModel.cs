6cccfa03d3ea9116c329280bb8c38bc47e836819
Виктория <babenko.viktoria.v@gmail.com>
2024-03-10 15:46:01 +0200

    Add project files.

---
+++ 8. Music portal/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace _8._Music_portal.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
