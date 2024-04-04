b8a34e702007b61d885ec63f4ec6e05e4874c7db
Виктория <babenko.viktoria.v@gmail.com>
2024-04-04 20:31:26 +0300

    #Task 5 Создание класса сущности исполнитель песни PerformerModel

---
+++ 8. Music portal/Models/PerformerModel.cs
@@ -0,0 +1,13 @@
+﻿namespace _8._Music_portal.Models
+{
+    public class PerformerModel
+    {
+        public int Id { get; set; }
+        public string? Name { get; set; }
+        public ICollection<SongsModel>? Songs { get; set; }
+        public PerformerModel()
+        {
+            this.Songs = new HashSet<SongsModel>();
+        }
+    }
+}
