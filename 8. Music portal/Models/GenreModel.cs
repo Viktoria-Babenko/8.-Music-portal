98a7e6572167df89893e49a39c236fdadb4cadf3
Виктория <babenko.viktoria.v@gmail.com>
2024-04-04 20:29:48 +0300

    #Task 4 Создание класса сущности жанр - GenreModel

---
+++ 8. Music portal/Models/GenreModel.cs
@@ -0,0 +1,13 @@
+﻿namespace _8._Music_portal.Models
+{
+    public class GenreModel
+    {
+        public int Id { get; set; }
+        public string? Name { get; set; }
+        public ICollection<SongsModel>? Songs { get; set; }
+        public GenreModel()
+        {
+            this.Songs = new HashSet<SongsModel>();
+        }
+    }
+}
