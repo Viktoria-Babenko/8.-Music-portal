898f8e72d80c871b0835dffeedd04985c440dd46
Виктория <babenko.viktoria.v@gmail.com>
2024-04-04 20:34:03 +0300

    #Task 6 Создание класса сущности песня SongsModel

---
+++ 8. Music portal/Models/SongsModel.cs
@@ -0,0 +1,17 @@
+﻿namespace _8._Music_portal.Models
+{
+    public class SongsModel
+    {
+        public int Id { get; set; }
+
+        public string? Name { get; set; }
+        public int? GenreID { get; set; }
+
+        public GenreModel? Genre { get; set; }
+        public int? PerformerID { get; set; }
+
+        public PerformerModel? Performer { get; set; }
+        public string? Track { get; set; }
+
+    }
+}
