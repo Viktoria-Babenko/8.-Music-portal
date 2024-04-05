7387f6bd7a46c39cbd23b29c95175b1bfd366222
Виктория <babenko.viktoria.v@gmail.com>
2024-04-05 15:50:06 +0300

    #Task 11 HomeController реализация авторизации и регистрации юзеров на сайте.
    Добавление страниц регистрации и авторизации , изминенние истраницы Index контролера HomeController

--- 8. Music portal/Controllers/HomeController.cs
+++ 8. Music portal/Controllers/HomeController.cs
@@ -1,21 +1,129 @@
 using _8._Music_portal.Models;
+using _8._Music_portal.NewFolder;
+using _8._Music_portal.Repository;
 using Microsoft.AspNetCore.Mvc;
+using NuGet.Protocol.Core.Types;
 using System.Diagnostics;
+using System.Security.Cryptography;
+using System.Text;
 
 namespace _8._Music_portal.Controllers
 {
     public class HomeController : Controller
     {
-        private readonly ILogger<HomeController> _logger;
+        IRepository repo;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(IRepository r)
         {
-            _logger = logger;
+            repo = r;
         }
-
-        public IActionResult Index()
+        public async Task<IActionResult> Index()
+        {
+            var model = await repo.GetAllSong();
+            return View(model);
+        }
+        public ActionResult Logout()
+        {
+            HttpContext.Session.Clear();
+            return RedirectToAction("Login", "Home");
+        }
+        public ActionResult Login()
         {
             return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Login(LoginModel logon)
+        {
+            if (ModelState.IsValid)
+            {
+                if (repo.GetUserCount() == 0)
+                {
+                    ModelState.AddModelError("", "Wrong login or password!");
+                    return View(logon);
+                }
+                var users = repo.UserWhere(logon?.Login);
+                if (users?.ToList().Count == 0)
+                {
+                    ModelState.AddModelError("", "Wrong login or password!");
+                    return View(logon);
+                }
+                var user = users?.First();
+                if (!user.Status.Value)
+                {
+                    ModelState.AddModelError("", "Your account is not authorized by the site administration!");
+                    return View(logon);
+                }
+                string? salt = user?.Salt;
+
+                //переводим пароль в байт-массив  
+                byte[] password = Encoding.Unicode.GetBytes(salt + logon.Password);
+
+                //вычисляем хеш-представление в байтах  
+                byte[] byteHash = SHA256.HashData(password);
+
+                StringBuilder hash = new StringBuilder(byteHash.Length);
+                for (int i = 0; i < byteHash.Length; i++)
+                    hash.Append(string.Format("{0:X2}", byteHash[i]));
+
+                if (user?.Password != hash.ToString())
+                {
+                    ModelState.AddModelError("", "Wrong login or password!");
+                    return View(logon);
+                }
+                HttpContext.Session.SetString("FirstName", user.FirstName);
+                HttpContext.Session.SetString("LastName", user.LastName);
+                HttpContext.Session.SetString("Login", user.Login);
+                return RedirectToAction("Index", "Home");
+            }
+            return View(logon);
+        }
+
+        public IActionResult Register()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Register(RegisterModel reg)
+        {
+            if (ModelState.IsValid)
+            {
+                UserModel user = new UserModel();
+                user.FirstName = reg.FirstName;
+                user.LastName = reg.LastName;
+                user.Login = reg.Login;
+                user.email = reg.email;
+                byte[] saltbuf = new byte[16];
+
+                RandomNumberGenerator randomNumberGenerator = RandomNumberGenerator.Create();
+                randomNumberGenerator.GetBytes(saltbuf);
+
+                StringBuilder sb = new StringBuilder(16);
+                for (int i = 0; i < 16; i++)
+                    sb.Append(string.Format("{0:X2}", saltbuf[i]));
+                string salt = sb.ToString();
+
+                //переводим пароль в байт-массив  
+                byte[] password = Encoding.Unicode.GetBytes(salt + reg.Password);
+
+                //вычисляем хеш-представление в байтах  
+                byte[] byteHash = SHA256.HashData(password);
+
+                StringBuilder hash = new StringBuilder(byteHash.Length);
+                for (int i = 0; i < byteHash.Length; i++)
+                    hash.Append(string.Format("{0:X2}", byteHash[i]));
+
+                user.Password = hash.ToString();
+                user.Salt = salt;
+                repo?.CreateUser(user);
+                repo?.Save();
+                return RedirectToAction("Login");
+            }
+
+            return View(reg);
         }
 
         public IActionResult Privacy()
