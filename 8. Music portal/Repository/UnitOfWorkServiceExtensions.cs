0bb8a7179f96c2f20900fd1f56ff567eb759c950
Виктория <babenko.viktoria.v@gmail.com>
2024-04-11 10:34:11 +0300

    #Task 31 Создание статического класса  UnitOfWorkServiceExtensions.
    С методом AddUnitOfWorkService метод расширения  сервиса AddScoped.

---
+++ 8. Music portal/Repository/UnitOfWorkServiceExtensions.cs
@@ -0,0 +1,16 @@
+﻿using _8._Music_portal.Models;
+using _8._Music_portal.NewFolder;
+
+namespace _8._Music_portal.Repository
+{
+    public static class UnitOfWorkServiceExtensions
+    {
+        public static void AddUnitOfWorkService(this IServiceCollection services)
+        {
+            services.AddScoped<IRepository, MyRepository>();
+            services.AddScoped<IRepository<GenreModel>, GenreRepositori>();
+            services.AddScoped<IRepository<PerformerModel>, PerformerRepositori>();
+
+        }
+    }
+}
