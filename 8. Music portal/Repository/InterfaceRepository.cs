97d6d10bed2287f59f67bfa0a8796fb3a2968b28
Виктория <babenko.viktoria.v@gmail.com>
2024-04-11 10:25:13 +0300

    #Task 28 Создание обобщенного интекфейса  IRepository<T> для репозиториев

---
+++ 8. Music portal/Repository/InterfaceRepository.cs
@@ -0,0 +1,15 @@
+﻿using _8._Music_portal.Models;
+
+namespace _8._Music_portal.Repository
+{
+    public interface IRepository<T> where T : class
+    {
+        Task<List<T>> GetAll();
+        Task<T> Get(int id);
+        Task Create(T item);
+        void Update(T item);
+        Task Delete(int id);
+        bool ModelExists(int id);
+        Task Save();
+    }
+}
