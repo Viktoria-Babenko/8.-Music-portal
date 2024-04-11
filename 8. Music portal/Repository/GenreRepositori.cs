e4667b0a8fe1736bd5cf39e576a21fc414e6eedc
Виктория <babenko.viktoria.v@gmail.com>
2024-04-11 10:28:20 +0300

    #Task 29 Создание репозиториев GenreRepositori и PerformerRepositori с реализацией интерфейса IRepository<T> для работы с классом контекст.

---
+++ 8. Music portal/Repository/GenreRepositori.cs
@@ -0,0 +1,54 @@
+﻿using _8._Music_portal.Models;
+using _8._Music_portal.NewFolder;
+using Humanizer.Localisation;
+using Microsoft.EntityFrameworkCore;
+using System.Numerics;
+
+namespace _8._Music_portal.Repository
+{
+    public class GenreRepositori : IRepository<GenreModel> 
+    {
+        private readonly MusicPortalContext _context;
+        public GenreRepositori(MusicPortalContext music) 
+        {
+            _context = music;
+        }
+        public async Task<List<GenreModel>> GetAll()
+        {
+            var item = await _context.Genres.ToListAsync();
+            return item;
+        }
+        public async Task<GenreModel> Get(int id)
+        {
+            GenreModel? genreModel = await _context.Genres
+                .FirstOrDefaultAsync(m => m.Id == id);
+            return genreModel;
+        }
+        
+        public async Task Create(GenreModel item)
+        {
+            _context.Add(item);
+            await _context.SaveChangesAsync();
+        }
+        public void Update(GenreModel item)
+        {
+            _context.Update(item);
+        }
+        public async Task Delete(int id)
+        {
+            GenreModel? genreModel = await _context.Genres
+               .FirstOrDefaultAsync(m => m.Id == id);
+            _context.Genres.Remove(genreModel);
+            await _context.SaveChangesAsync();
+        }
+        public bool ModelExists(int id)
+        {
+            return _context.Genres.Any(e => e.Id == id);
+        }
+        public async Task Save()
+        {
+            await _context.SaveChangesAsync();
+        }
+
+    }
+}
---
+++ 8. Music portal/Repository/PerformerRepositori.cs
@@ -0,0 +1,50 @@
+﻿using _8._Music_portal.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace _8._Music_portal.Repository
+{
+    public class PerformerRepositori : IRepository<PerformerModel>
+    {
+        private readonly MusicPortalContext _context;
+        public PerformerRepositori(MusicPortalContext music)
+        {
+            _context = music;
+        }
+        public async Task<List<PerformerModel>> GetAll()
+        {
+            var item = await _context.Performers.ToListAsync();
+            return item;
+        }
+
+        public async Task<PerformerModel> Get(int id)
+        {
+            PerformerModel? performerModel = await _context.Performers
+                .FirstOrDefaultAsync(m => m.Id == id);
+            return performerModel;
+        }
+        public async Task Create(PerformerModel item)
+        {
+            _context.Add(item);
+            await _context.SaveChangesAsync();
+        }
+        public void Update(PerformerModel item)
+        {
+            _context.Update(item);
+        }
+        public async Task Delete(int id)
+        {
+            PerformerModel? performerModel = await _context.Performers
+               .FirstOrDefaultAsync(m => m.Id == id);
+            _context.Performers.Remove(performerModel);
+            await _context.SaveChangesAsync();
+        }
+        public bool ModelExists(int id)
+        {
+            return _context.Performers.Any(e => e.Id == id);
+        }
+        public async Task Save()
+        {
+            await _context.SaveChangesAsync();
+        }
+    }
+}
